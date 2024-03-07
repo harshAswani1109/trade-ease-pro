d5f35748582c860721e5094b96657a12b6ef4aa5
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2024-03-07 10:33:51 +0530

    added buttons

--- components/MonitorPopup/index.js
+++ components/MonitorPopup/index.js
@@ -38,12 +38,42 @@ const MonitorPopup = ({
           </button>
         </div>
       </div>
-      <div className="absolute top-0 right-0 border-2 border-black bg-white h-40 w-40">
-        Looking for pattern to invest
+      <div className="absolute top-8 right-2   ">
+        <button
+          type="button"
+          className="text-white flex gap-2   bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 40 40"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#000000"
+            className="animate-spin"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <g transform="translate(2 2)" stroke-width="4">
+                <circle
+                  stroke-opacity=".5"
+                  cx="18"
+                  cy="18"
+                  r="18"
+                  className="text-white "
+                ></circle>
+                <path d="M36 18c0-9.94-8.06-18-18-18"></path>
+              </g>
+            </g>
+          </svg>
+          Looking for pattern to invest
+        </button>
       </div>
-      <div className="absolute bottom-0 right-0 border-2 border-black bg-white h-40 w-40">
-        <button className="w-full h-full" onClick={handleHowItWorksClick}>
-          How it works
+      <div className="absolute bottom-8 right-8">
+        <button
+          type="button"
+          className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={handleHowItWorksClick}
+        >
+          Check Working
         </button>
       </div>
 
@@ -56,7 +86,7 @@ const MonitorPopup = ({
               className="max-w-full max-h-full mb-4"
             />
             <p className="text-lg font-semibold">
-              Rounding Bottom pattern detected, Start investing you Money
+              Rounding Bottom pattern detected, Start investing your Money
             </p>
             <button
               className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:outline-none focus:shadow-outline-green mt-4"
